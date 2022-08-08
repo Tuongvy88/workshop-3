9217c152d7740261e82ebf796b3ee57f9422ed37
Tuongvy88 <109933390+Tuongvy88@users.noreply.github.com>
2022-08-08 12:05:55 +0930

    Add files via upload

---
+++ funcrion-1-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+void print_class(string courses[4], string students[], int report_card[][4], int nstudents) {
+
+cout << "Report Card: ";
+
+for(int i = 0; i<4; i++) {
+
+cout << courses[i] << " ";
+
+}
+
+cout << endl;
+
+for(int i=0; i<nstudents; i++) {
+
+cout << students[i] << " ";
+
+for(int j=0; j<4; j++) {
+
+cout << report_card[i][j] << " ";
+
+}
+
+cout << endl;
+
+}
+
+}
